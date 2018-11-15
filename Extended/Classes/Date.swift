9ade6dad0a225ad5a056d151788b7593fd3309ae
amirshayegh <shayegh@me.com>
2018-11-15 11:05:14 -0800

    added a date extension

--- Extended/Classes/Date.swift
+++ Extended/Classes/Date.swift
@@ -85,4 +85,17 @@ extension Date {
     public func startOf() -> Date?{
         return Calendar(identifier: .gregorian).startOfDay(for: self)
     }
+
+    public func setTime(hour: Int, min: Int, sec: Int, timeZoneAbbrev: String = "UTC") -> Date? {
+        let x: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
+        let cal = Calendar.current
+        var components = cal.dateComponents(x, from: self)
+
+        components.timeZone = TimeZone(abbreviation: timeZoneAbbrev)
+        components.hour = hour
+        components.minute = min
+        components.second = sec
+        
+        return cal.date(from: components)
+    }
 }
